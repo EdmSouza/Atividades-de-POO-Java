03690b7f8aa9f5ae41ffbebc576cad1f5b207cd9
EdmSouza <eduardoguimara321@gmail.com>
2025-11-27 12:50:58 -0300

    Estrutura Last-In, First-Out em JAVA jdk25
    Pilha em poo funcionando, detalhes da questao: Defina uma classe que implemente a estrutura de dados Pilha. Nesta estrutura, o último que entra é o primeiro em sair. As instâncias devem ter uma capacidade máxima. Após atingir esta capacidade, nenhum empilhamento poderá ser feito. Antes de implementar em Java, descreva a classe com um diagrama.

--- ARRAYS/Pilha.java
+++ ARRAYS/Pilha.java
@@ -4,38 +4,71 @@ public class Pilha {
     private int qtd;
 
 
-    public Pilha(int elemtopo, int qtd){
+    public Pilha(){
         qtd = 0;
         elemtopo = -1;
         pilha = new int[100];
     }
 
-    public void desempilhar(){
+    public int desempilhar(){
         if(qtd == 0){
-            IO.println("Pilha está vazia, não é possível desempilhar.");
+            IO.println("A pilha está vazia, não é possível desempilhar.\n");
+            return -1;
         }else{
+            int valordesempilhado = pilha[elemtopo];
             elemtopo--;
             qtd--;
+            IO.println("O elemento foi desempilhado com sucesso!\n");
+            return valordesempilhado;
         }
     }
 
     public void empilhar(int elemt){
-        if(pilha.length()<(qtd -1)){
+        if(pilha.length>qtd){
             elemtopo++;
             pilha[elemtopo] = elemt;
             qtd++;
+            IO.println("O número: "+elemt+" foi empilhado com sucesso!\n");
         }else{
-            IO.println("A pilha está cheia, não é possível empilhar");
+            IO.println("A pilha está cheia, não é possível empilhar.\n");
         }
     }
 
     public void showpilha(){
-        IO.println(pilha[elemtopo]);
+        if(qtd == 0){
+            IO.println("A pilha está vazia, adicione algum número inteiro primeiro.");
+        }else{
+            IO.println("\nO(s) elemento(s) da pilha é(são): \n");
+            for(int i = qtd-1; i>=0;i--){
+                IO.println("["+pilha[i]+"]"+"");
+        }
+    }
     }
 
 
     public static void main(String[] args){
-        Pilha main = new Pilha()
+        Pilha main = new Pilha();
+        while(true){
+            IO.println("\n---PROGRAMA DE PILHA---\n");
+            int opcao = Integer.parseInt(IO.readln("Digite 1 para emplihar, 2 para desempilhar ou 3 para mostrar a pilha.\nCaso queira encerrar o programa, digite 0.\n\n"));
+            if(opcao == 1){
+                int elemt = Integer.parseInt(IO.readln("\n*****Digite um número inteiro para empilhar*****\n"));
+                main.empilhar(elemt);
+                }else if(opcao == 2){
+                    int num = main.desempilhar();
+                    if(num != -1){
+                        IO.println("Você removeu o número: "+num);
+                    }else{
+                        IO.println("");
+                    }
+                }else if(opcao == 3){
+                    main.showpilha();
+                }else if(opcao == 0){
+                    IO.println("\nEncerrando o programa...");
+                    break;
+                }else{
+                    IO.println("Digite um número válido");
+                }
+            }
+        }
     }
-
-}
